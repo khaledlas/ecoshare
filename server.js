baddc0454b0e4ec16cc2c5a9e77c79696b4eef13
khaledlas <khaledlas@gmail.com>
2022-05-23 23:27:02 +0100

    heroku3

--- server.js
+++ server.js
@@ -21,15 +21,15 @@ app.use("/api/quidproquos", quidproquoRouter);
 // });
 
 // server static assets if in production
-if (
-  process.env.NODE_ENV === "production" ||
-  process.env.NODE_ENV === "staging"
-) {
-  app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname + "/client/build/index.html"));
-  });
-}
+// if (
+//   process.env.NODE_ENV === "production" ||
+//   process.env.NODE_ENV === "staging"
+// ) {
+//   app.use(express.static("client/build"));
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname + "/client/build/index.html"));
+//   });
+// }
 
 app.listen(
   process.env.PORT,
