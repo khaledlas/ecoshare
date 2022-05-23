bb10bf63808ff223700d64e915d61c819fa188a5
khaledlas <khaledlas@gmail.com>
2022-05-24 00:57:05 +0100

    heroku3

--- server.js
+++ server.js
@@ -26,10 +26,10 @@ app.use("/api/quidproquos", quidproquoRouter);
 //   process.env.NODE_ENV === "production" ||
 //   process.env.NODE_ENV === "staging"
 // ) {
-//   app.use(express.static("client/build"));
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.join(__dirname + "/client/build/index.html"));
-//   });
+app.use(express.static(path.join(__dirname, "client/build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/client/build/index.html"));
+});
 // }
 
 app.listen(
